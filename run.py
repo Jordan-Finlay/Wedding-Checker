8708c078fe75f849ca6ab6dc46347ca30ba8dc2c
Jordan-Finlay <>
2024-01-28 10:07:08 +0000

    updated validate name checker

--- run.py
+++ run.py
@@ -2,27 +2,37 @@ def enter_name():
     """
     Asks user to input name to check he/she is on the wedding list
     """
+    while True:
+        print("Please enter your name")
 
-    print("Please enter your name")
+        name_str = input("Enter your name here.\n")
 
-    name_str = input("Enter your name here.\n")
+        if validate_name(name_str):
+            print(f"Welcome to the wedding '{name_str}'")
+            break
 
-    print(f"name provided is {name_str}")
-    
-    validate_name(name_str)
+    return name_str
+
 
 
 def validate_name(name_str):
     """
-    This is used to check the name inputted is correct,
-    Making sure there are only letters
+    This is used to check the name inputted is correct
     """
-    try:
-        if len(name_str) == 1:
-            raise ValueError(
-                f"Name is too short"
-            )
-    except ValueError as e:
-        print(f"Invalid Name: {e}, please try again.\n")
+    if not name_str.isalpha():
+        print(f"You entered : '{name_str}'.")
+        print("You cannot use numbers")
+        print("This is not a valid name.\n")
+        return False
+        
+    else:
+        return True
 
-enter_name()
+
+def main():
+    """
+    Runs all program functions
+    """
+    enter_name()
+
+main()

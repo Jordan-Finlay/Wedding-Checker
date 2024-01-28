821d37f590993c359c096dd737a2e2d208d0e6e2
Jordan-Finlay <>
2024-01-28 09:16:43 +0000

    set up enter name function

--- run.py
+++ run.py
@@ -1,3 +1,14 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+def enter_name():
+    """
+    Asks user to input name to check he/she is on the wedding list
+    """
+
+    print("Please enter your name")
+
+    name_str = input("Enter your name here")
+
+    print(f"name provided is {name_str}")
+  
+
+
+enter_name()

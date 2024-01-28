5c7fd1af30971fd69124e0a51c085627edab263e
Jordan-Finlay <>
2024-01-28 09:36:49 +0000

    added validate name check

--- run.py
+++ run.py
@@ -5,10 +5,24 @@ def enter_name():
 
     print("Please enter your name")
 
-    name_str = input("Enter your name here")
+    name_str = input("Enter your name here.\n")
 
     print(f"name provided is {name_str}")
-  
+    
+    validate_name(name_str)
 
+
+def validate_name(name_str):
+    """
+    This is used to check the name inputted is correct,
+    Making sure there are only letters
+    """
+    try:
+        if len(name_str) == 1:
+            raise ValueError(
+                f"Name is too short"
+            )
+    except ValueError as e:
+        print(f"Invalid Name: {e}, please try again.\n")
 
 enter_name()
